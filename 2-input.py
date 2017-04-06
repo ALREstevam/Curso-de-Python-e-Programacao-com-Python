e7a1ea25c07410a122516ec8411768fd77d5e9ee
ALREstevam <a166348@g.unicamp.com>
2017-04-06 15:36:16 -0300

    Adicionando arquivos mostrando conceitos muito iniciais

---
+++ 2-input.py
@@ -0,0 +1,4 @@
+nome = input('Qual o seu nome? ')
+idade = input('Qual sua idade? ')
+peso = input('Qual seu peso? ')
+print('Olá sr(a) ', nome, "pelo visto você pesa", peso, "kg e tem ", idade, "anos")
---
+++ 5-Soma.py
@@ -0,0 +1,4 @@
+n1 = input('n1: ')
+n2 = input('n2: ')
+sum = float(n1) + float(n2)
+print(sum)
