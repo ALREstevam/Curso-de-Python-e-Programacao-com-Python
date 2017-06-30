c4a2d3a24f7baea774b3cf3556ed1e387fb33142
ALREstevam <a166348@g.unicamp.com>
2017-06-30 10:07:39 -0300

    Criando pastas e adicionando ex do livro

---
+++ Introdução à programação com python/Exercícicios/Exercicio 2.1.py
@@ -0,0 +1,9 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> (10+20)*30
+900
+>>> (4**2)/30
+0.5333333333333333
+>>> ((9**4) + 2)*(6-1)
+32815
+>>> 
